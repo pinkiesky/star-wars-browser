6853b703df5149d6a54940bfe7c4625fe0a5f971
Alexander Zinin <spore_09@mail.ru>
2019-07-03 08:54:26 +0900

    add: Surface component

---
+++ src/components/Surface.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="surface">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="stylus" scoped>
+.surface
+  border 1px solid #eee
+  border-radius 3px
+
+  margin 4px
+  padding 8px
+
+  box-shadow 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)
+  background-color white
+</style>
