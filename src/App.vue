c2402c661217b4bdbb513d84f500e1330b3ec326
Alexander Zinin <spore_09@mail.ru>
2019-07-04 00:22:20 +0900

    add: nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
-    <h1>Star Wars Browser</h1>
+    <nav>
+      <h1>Star Wars Browser</h1>
+      <router-link to="/">Characters</router-link>
+      <router-link to="/ss">Spaceships</router-link>
+      <router-link to="/films">Films</router-link>
+    </nav>
     <router-view/>
   </div>
 </template>
@@ -12,4 +17,19 @@
 
   @media screen and (min-width: 800px)
     max-width 800px
+
+nav
+  padding 8px 0
+  display flex
+  flex-direction row
+  align-items baseline
+
+  @media screen and (max-width: 600px) {
+    justify-content center
+    align-items center
+    flex-direction column
+  }
+
+  > *
+    margin 0 6px
 </style>
