0ec42fa6b85f893f1f603c472e844265da5f6d17
Alexander Zinin <spore_09@mail.ru>
2019-07-03 23:57:41 +0900

    improve: limit app container max width

--- src/App.vue
+++ src/App.vue
@@ -4,3 +4,12 @@
     <router-view/>
   </div>
 </template>
+
+<style lang="stylus" scoped>
+#app
+  width 100%
+  margin 0 auto
+
+  @media screen and (min-width: 800px)
+    max-width 800px
+</style>
