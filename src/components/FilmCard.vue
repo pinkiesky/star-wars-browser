aa018c234db0ca1b8c981f92bf97e11e969ab79b
Alexander Zinin <spore_09@mail.ru>
2019-07-03 23:00:26 +0900

    add: FilmCard

---
+++ src/components/FilmCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <Surface width="350px">
+    <h3>{{ film.title }}</h3>
+    <LabeledData label="director">{{ film.director }}</LabeledData>
+    <LabeledData label="producer">{{ film.producer }}</LabeledData>
+    <LabeledData label="release">{{ film.release_date }}</LabeledData>
+    <LabeledData label="description">{{ film.opening_crawl }}</LabeledData>
+  </Surface>
+</template>
+
+<script>
+import Surface from './Surface';
+import LabeledData from './LabeledData';
+
+export default {
+  props: {
+    film: {
+      type: Object,
+      required: true,
+    }
+  },
+  components: {
+    Surface, LabeledData,
+  },
+}
+</script>
