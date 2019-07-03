e30fa8b54a05be784a590f38dd58a8cd2736958f
Alexander Zinin <spore_09@mail.ru>
2019-07-03 09:54:39 +0900

    improve: add size support for Surface

--- src/components/Surface.vue
+++ src/components/Surface.vue
@@ -1,8 +1,32 @@
 <template>
-  <div class="surface">
+  <div class="surface" :style="styles">
     <slot></slot>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    width: {
+      type: String,
+      default: 'auto',
+    },
+    height: {
+      type: String,
+      default: 'auto',
+    },
+  },
+  computed: {
+    styles() {
+      return {
+        width: this.width,
+        height: this.height,
+      };
+    },
+  },
+};
+</script>
+
 
 <style lang="stylus" scoped>
 .surface
@@ -11,6 +35,8 @@
 
   margin 4px
   padding 8px
+
+  box-sizing border-box
 
   box-shadow 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)
   background-color white
